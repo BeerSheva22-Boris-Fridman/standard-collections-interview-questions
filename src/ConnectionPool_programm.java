7d9ce4567266627226d543a1e4f697519b053a3f
Kulicod <fridman1986@gmail.com>
2023-03-02 13:44:16 +0200

    HW 35

---
+++ src/ConnectionPool_programm.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class ConnectionPool_programm implements ConnectionsPool {
+	LinkedList<Object> pool = new LinkedList<Object>();
+	HashMap<Integer, Object> connections = new HashMap<Integer, Object>();
+
+	private int sizeOfPool = 5;
+
+	@Override
+	public boolean addConnection(Connection connection) {
+		boolean res = false;
+		int id = connection.getId();
+		if (!connections.containsKey(id)) {
+			pool.addFirst(connection);
+			connections.put(id, connection);
+			res = true;
+		}
+		if (pool.size() > sizeOfPool) {
+			removeOldestConnectionInPool();
+//			pool.removeLast();
+//			Object removedID = pool.removeLast();
+//			connections.remove(removedID);
+		}
+		return res;
+	}
+
+	private void removeOldestConnectionInPool() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Connection getConnection(int id) {
+		if (connections.containsKey(id)) {
+			Connection res = (Connection) connections.get(id);
+			pool.remove(res);
+			pool.add(res);
+			return res;
+		}
+		return null;
+	}
+
+}
